cf1d72ad966b778ced1560c896872a5003bcc72c
GargShreyansh07 <93382627+GargShreyansh07@users.noreply.github.com>
2022-05-31 11:31:02 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,34 @@
+const accessCode1 = "GOLD";
+const accessCode2 = "NECKLACE";
+const accessCode3 = "DIAMOND";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("O L G D", 100,50)
+    fill("yellow")
+    text("Hint: It is something very shiny", 100,70)
+    text("And is a valuable metal", 100,90)
+    text("If you win at the Olympics", 100,110)
+    text("It is the color of your medal!!", 100,130)
+
+    fill("white")
+    textSize(15)
+    text("K A E N C L E C", 700,150)
+    fill("hotpink")
+    text("Hint: I am usually shiny", 700,170)
+    text("In a jeweller’s I am sold  ", 700,190)
+    text("I sometimes hold a pendant ", 700,210)
+    text("I’ve a chain that’s made of gold !!", 700,230)
+
+    fill("white")
+    textSize(15)
+    text("M I O D N D A", 100,270)
+    fill("limegreen")
+    text("Hint: I’m a type of red card", 100,290)
+    text("But I am not a heart", 100,310)
+    text("Of an engagement ring", 100,330)
+    text("I am usually part", 100,350)
+
+}
